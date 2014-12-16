cdeaca37dfd8b3d01b54f5f1c744166c280600e6
Jerry OBrien <jerobie1@comcast.net>
2014-12-16 15:43:36 -0500

    Modifications made as per Assignment 2.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.r has two primary functions makeCacheMatrix and cachesolve
+##
+##Together these functions provide for a one time matrix inversion (solve) with results 
+##and the ability to subsequently retrieve results without requiring to re-execute a solve.
 
-## Write a short comment describing this function
+
+## makeCacheMatrix is a function of functions establishing a link between three functions, 
+## "get", "setsolve" and "getsolve", and a provided matrix.. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    s <- NULL                               # clear object "s"
+    get <- function() x                     # define "get"function
+    setsolve <- function(solve) s <<- solve # define "setsolve" function
+    getsolve <- function() s                # define "getsolve" function
+    list(get = get,                         # provide list of the available/associated functions
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+##cachesolve makes use of the makeChcheMatrix functions to  calculate and cache the 
+##matrix inversion results and subsequently retrieve the cached results, in lieu of recalculation. 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    s <- x$getsolve()                      # call getsolve to load cache of solve(data) of "x" - "s"
+    if(!is.null(s)) {                      # if cache solve(data) of "x" exist use cache
+        message("getting cached data")     # and display/return the results
+        return(s)
+    }
+    data <- x$get()                        # if not - - store the associated matrix in data
+    s <- solve(data, ...)                  # solve for inverse matrix
+    x$setsolve(s)                          # cache the solve
+    s                                      # return the result
 }
